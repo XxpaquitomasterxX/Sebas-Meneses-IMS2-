73c4c047535ef669fa4071c8a7e3e86282301dbd
Xxpaquitomaster <89699500+XxpaquitomasterxX@users.noreply.github.com>
2021-11-23 15:13:32 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() { 
+int casa=0; 
+int carro=0;
+
+cout<< "digite un numero:  "; 
+
+cin>>casa;  
+
+cout<<" digite un numero:  "; 
+
+cin>>carro; 
+
+ if (casa>carro) { 
+
+cout<<casa <<"  es mayor que  " << carro; 
+
+} 
+
+else { 
+
+cout<<casa <<"  es menor que  " << carro; 
+
+} }
